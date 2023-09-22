425fb79baed762a35af8378a11e716b195c43f53
Gina Medina <ga.medina0613@gmail.com>
2023-09-22 18:59:54 +0100

    moved files to Phase One repository

--- test/test_counter.py
+++
@@ -1,5 +0,0 @@
-from lib.counter import *
-
-def test_counter_in_cero():
-    little_counter = Counter()
-    assert little_counter.report() == "Counted to 0 so far."
--- test/test_report_length.py
+++
@@ -1,5 +0,0 @@
-from lib.report_length import *
-
-def test_report_length_python():
-    result = report_length("python")
-    assert result == "This string was 6 characters long."
