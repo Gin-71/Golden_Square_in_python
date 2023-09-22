5ad357543f0752013d6416ab7df20a6aed99eae4
Hilary Smith <hsmith98@hotmail.co.uk>
2023-09-22 16:34:17 +0100

    Pairing With Gina

--- my_diary/lib/diary.py
+++ my_diary/lib/diary.py
@@ -1,7 +1,8 @@
+from lib.diary_entry import DiaryEntry
+
 class Diary:
     def __init__(self):
-        # entries_list 
-        pass
+        self.the_list = []
 
     def add(self, entry):
         # Parameters:
@@ -10,19 +11,31 @@ class Diary:
         #   Nothing
         # Side-effects:
         #   Adds the entry to the entries list
-        pass
+        if type(entry) != DiaryEntry:
+            raise Exception('The entry should be diary_empty type')
+        elif entry.contents == '' and entry.title == '':
+            raise Exception('Nothing to add!')
+        elif entry.contents == '':
+            raise Exception('Empty contents!')
+        elif entry.title == '':
+            raise Exception('Empty title!')
+        else:
+            self.the_list.append(entry)
 
     def all(self):
         # Returns:
         #   A list of instances of DiaryEntry
-        pass
+        return self.the_list
 
     def count_words(self):
         # Returns:
         #   An integer representing the number of words in all diary entries
         # HINT:
         #   This method should make use of the `count_words` method on DiaryEntry.
-        pass
+        counter = 0
+        for entry in self.the_list:
+            counter += entry.count_words()
+        return counter
 
     def reading_time(self, wpm):
         # Parameters:
@@ -31,7 +44,11 @@ class Diary:
         # Returns:
         #   An integer representing an estimate of the reading time in minutes
         #   if the user were to read all entries in the diary.
-        pass
+        words = self.count_words()
+        if words % wpm == 0:
+            return int(words / wpm)
+        else:
+            return int(words / wpm) + 1
 
     def find_best_entry_for_reading_time(self, wpm, minutes):
         # Parameters:
@@ -43,4 +60,11 @@ class Diary:
         #   An instance of DiaryEntry representing the entry that is closest to,
         #   but not over, the length that the user could read in the minutes
         #   they have available given their reading speed.
-        pass
+        entry_words = 0
+        my_favourite_entry = ''
+        words = wpm * minutes
+        for entry in self.the_list:
+            if entry.count_words() <= words and entry.count_words() > entry_words:
+                my_favourite_entry = entry
+                entry_words = entry.count_words()
+        return my_favourite_entry
--- my_diary/tests/test_integration.py
+++ my_diary/tests/test_integration.py
@@ -1,18 +1,110 @@
+import pytest
+from lib.diary_entry import *
+from lib.diary import *
+
 """
 Input / Output diagram
 
 add one entry and check the entries list, type DiaryEntry ==> entries_list
 add two entries and check the entries list, type DiaryEntry ==> entries_list
+"""
+def test_one_entries_list():
+    first_diary = Diary()
+    first_entry = DiaryEntry('Friday 22', 'Pairing Session With Hilary')
+    first_diary.add(first_entry)
+    assert first_diary.all() == [first_entry]
+
+def test_two_entries_list():
+    first_diary = Diary()
+    first_entry = DiaryEntry('Friday 22', 'Pairing Session With Hilary')
+    second_entry = DiaryEntry('Thursday 21', 'Pairing with Catherine')
+    first_diary.add(first_entry)
+    first_diary.add(second_entry)
+    assert first_diary.all() == [first_entry, second_entry]
+
+"""
 
 add one entry and count the words ==> numbers of words - int / use count_words from diary_entry
 add two entries and count the words ==> numbers of words in both entries together - int / use count_words from diary_entry
+"""
+def test_count_words_one_entry():
+    first_diary = Diary()
+    first_entry = DiaryEntry('Friday 22', 'Pairing Session With Hilary')
+    first_diary.add(first_entry)
+    assert first_diary.count_words() == 4
 
+def test_count_words_two_entries():
+    first_diary = Diary()
+    first_entry = DiaryEntry('Friday 22', 'Pairing Session With Hilary')
+    second_entry = DiaryEntry('Thursday 21', 'Pairing with Catherine')
+    first_diary.add(first_entry)
+    first_diary.add(second_entry)
+    assert first_diary.count_words() == 7
+
+"""
 add one entry and return the reading time ==> reading time - int / use count_words from diary_entry
 add two entries and return the reading time ==> reading time - int / use count_words from diary_entry
+"""
+def test_one_entry_reading_time():
+    first_diary = Diary()
+    first_entry = DiaryEntry('Friday 22', 'Pairing With Hilary')
+    first_diary.add(first_entry)
+    assert first_diary.reading_time(3) == 1
 
+def test_two_entries_reading_time():
+    first_diary = Diary()
+    first_entry = DiaryEntry('Friday 22', 'Pairing Session With Hilary')
+    second_entry = DiaryEntry('Thursday 21', 'Pairing with Catherine')
+    first_diary.add(first_entry)
+    first_diary.add(second_entry)
+    assert first_diary.reading_time(2) == 4
+
+"""
 add two entries and return the best entry ==> entry - Diary_entry / use count_words from diary_entry
+"""
+def test_best_entry():
+    first_diary = Diary()
+    first_entry = DiaryEntry('Friday 22', 'Pairing Session With Hilary')
+    second_entry = DiaryEntry('Thursday 21', 'Pairing with Catherine')
+    first_diary.add(first_entry)
+    first_diary.add(second_entry)
+    assert first_diary.find_best_entry_for_reading_time(1, 4) == first_entry
 
+"""
 edge cases
 empty string ==> Exception "Empty entry is not valid"
+
+"""
+def test_empty_contents():
+    first_diary = Diary()
+    first_entry = DiaryEntry('Friday 22', '')
+    with pytest.raises(Exception) as e:
+        first_diary.add(first_entry)
+    error_message = str(e.value)
+    assert error_message == 'Empty contents!'
+
+def test_empty_title():
+    first_diary = Diary()
+    first_entry = DiaryEntry('', 'Pairing Session With Hilary')
+    with pytest.raises(Exception) as e:
+        first_diary.add(first_entry)
+    error_message = str(e.value)
+    assert error_message == 'Empty title!'
+
+def test_empty_entry():
+    first_diary = Diary()
+    first_entry = DiaryEntry('', '')
+    with pytest.raises(Exception) as e:
+        first_diary.add(first_entry)
+    error_message = str(e.value)
+    assert error_message == 'Nothing to add!'
+
+def test_wrong_format():
+    first_diary = Diary()
+    with pytest.raises(Exception) as e:
+        first_diary.add('not correctly')
+    error_message = str(e.value)
+    assert error_message == 'The entry should be diary_empty type'
+"""
 wrong format ==> Exception "The entry should be diary_empty type"
 """
